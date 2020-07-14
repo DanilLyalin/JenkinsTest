8b202dff10eafcafebf1783e2149457d2e9e1e30
DanilLyalin <danil.lyalin@gmail.com>
2020-07-14 20:31:35 +0300

    Added robot support

--- calc.py
+++ calc.py
@@ -1,18 +1,21 @@
+import sys
 
-while True:
-    op = input("Введите знак операции (+,-,*,/) или exit для выхода: ")
-    if op == 'exit':
-        break
-    if op in ('+', '-', '*', '/'):
-        x = float(input("Введите x: "))
-        y = float(input("Введите y: "))
-        if op == '+':
-            print("Ответ: %.2f" % (x+y))
-        elif op == '-':
-            print("Ответ: %.2f" % (x-y))
-        elif op == '*':
-            print("Ответ: %.2f" % (x*y))
-        elif op == '/':
-            print("Ответ: %.2f" % (x/y))
-    else:
-        print("Неверный параметр!")
+args = sys.argv
+if len(args) != 4:
+	print("Неверный параметр!")
+else:
+	op,x,y = args[1],float(args[2]),float(args[3])
+	if op in ('+', '-', '*', '/'):
+		if op == '+':
+			print("%.10f" % (x+y))
+		elif op == '-':
+			print("%.10f" % (x-y))
+		elif op == '*':
+			print("%.10f" % (x*y))
+		elif op == '/':
+			if y == 0:
+				print("Деление на ноль!")
+				exit()
+			print("%.10f" % (x/y))
+	else:
+		print("Неверный параметр!")
